6d95777cdb2b6bbe694da01162f648653e30e788
Metehan Gürgentepe <73113420+metehangurgentepe@users.noreply.github.com>
2024-09-01 13:38:51 +0300

    StormViewer App swift challenge

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Metehan Gürgentepe on 20.08.2024.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                print(item)
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
+
