6d80d59626c3c7cecc9dc34dafa0bfb7c294ecf1
Metehan Gürgentepe <73113420+metehangurgentepe@users.noreply.github.com>
2024-09-01 14:01:22 +0300

    JavascriptInjection Extension for swift challenge.
    This is a extension for safari application.

---
+++ JavascriptInjection/Extension/ActionViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Metehan Gürgentepe on 27.08.2024.
+//
+
+import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
---
+++ JavascriptInjection/JavascriptInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JavascriptInjection
+//
+//  Created by Metehan Gürgentepe on 27.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
