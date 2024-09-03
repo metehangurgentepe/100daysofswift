9598eec8b94c53bc12c35fe91ca6f554ec13afdc
Metehan Gürgentepe <73113420+metehangurgentepe@users.noreply.github.com>
2024-09-03 10:59:58 +0300

    ConcentrationGame for swift challenge

---
+++ ConcentrationGame/ConcentrationGame/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  ConcentrationGame
+//
+//  Created by Metehan Gürgentepe on 3.09.2024.
+//
+
+import Foundation
+
+struct Card {
+    var emoji: String
+    var isHidden: Bool
+}
